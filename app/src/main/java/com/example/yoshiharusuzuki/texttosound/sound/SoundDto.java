44efc7a9df1fb5be9688d26302c8e381c9a0dd91
yoshiharusuzuki <suzuki.yo@opst.co.jp>
2016-02-02 08:31:26 +0900

    リファクタリング、定数の外だし

--- app/src/main/java/com/example/yoshiharusuzuki/texttosound/sound/SoundDto.java
+++ app/src/main/java/com/example/yoshiharusuzuki/texttosound/sound/SoundDto.java
@@ -2,19 +2,17 @@ package com.example.yoshiharusuzuki.texttosound.sound;
 
 public class SoundDto {
 
-	// 音声データ
-	private short[] sound;
-	
-	/**
-	 * 引数付きコンストラクタ
-	 * @param source
-	 * @param length
-	 */
-	public SoundDto(short[] source) {
-		this.sound = source;
-	}
-	
-	public short[] getSound() {
-		return sound;
-	}
+    // 音声データ
+    private short[] sound;
+
+    /**
+     * 引数付きコンストラクタ
+     */
+    public SoundDto(short[] source) {
+        this.sound = source;
+    }
+
+    public short[] getSound() {
+        return sound;
+    }
 }
